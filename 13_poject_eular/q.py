6b6bbc10474a5118c3b7a313cf58d9a5b283d9e6
pakusadewa <gede.pakusadewa@gmail.com>
2021-09-11 21:48:06 +0800

    [update 13] done making and testing diagonal function

--- 13_poject_eular/q.py
+++ 13_poject_eular/q.py
@@ -17,7 +17,29 @@ def find_diagonal_value_left_right(data, row, col, nDiagonal):
         for x in range(nDiagonal):
             total = total * data[row + x][col + x]
     return total
-    
+   
+def find_diagonal_value_right_left(data, row, col, nDiagonal):
+    total = -11111111
+    isColGood = False
+    isRowGood = False
+
+    if (col-nDiagonal) >= -1 :
+        isColGood = True
+       # print(True)
+    if (isColGood == True) & ((row + nDiagonal) <= len(data)):
+        total = 1
+        for x in range(nDiagonal):
+            total = total * data[row + x][col - x]
+    return total
+
+def find_value_diagonally(data, nDiagonal):
+    v = -1
+    for row in range(len(data)):
+        for col in range(len(data[0])):
+           # v = (find_diagonal_value_right_left(data, row, col, nDiagonal))
+            v = find_diagonal_value_left_right(data, row, col, nDiagonal)
+            print('row -> ' + str(row) + ' col -> ' + str(col)+ ' == '+ str(v))
+
 
 dt = [
         [1,   2,  3,  4, 90], 
@@ -25,5 +47,12 @@ dt = [
         [9,  10, 11, 12, 92],
         [13, 14, 15, 16, 92]
     ]
+dt2 = [
+        [0, 1, 9, 1, 0],
+        [3, 0, 0, 1, 1],
+        [9, 7, 2, 1, 9],
+        [3, 4, 6, 1, 9]
+    ]
 
-print('-> ' + str(find_diagonal_value_left_right(dt, 1, 2, 3)))
+find_value_diagonally(dt2, 3)
+#print('-> ' + str(find_diagonal_value_right_left(dt, 2, 4, 2)))i

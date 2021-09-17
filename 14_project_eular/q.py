fee4950f4b22d229f3e1609a33ce0a4916078551
pakusadewa <gede.pakusadewa@gmail.com>
2021-09-17 10:16:25 +0800

    [update 14] add another function
    function to eliminate duplicate number
    in list pytho

--- 14_project_eular/q.py
+++ 14_project_eular/q.py
@@ -4,8 +4,7 @@
 #2 make a factorisation function to search 5000 divisors from triable number
 #3 
 
-//continue to test gf() function then test it then make function
-// to generate dactor number 
+//lanjut benerin delN(), alih adi list.remove sink nyak berfungsi???
 
 # function to calculated and return triangle number
 # from one natural number
@@ -18,18 +17,41 @@ def gt(n):
 #function to get sum of divisors from one number
 def gf(n):
     i = 0
-    tmp = [1]
-
+    tmp = [1, n]
     for x in range(n, 0, -1):
+        if x == 1 or x == 6:
+            continue
         if n%x == 0:
-            #tmp[i] = n/x
-            #tmp[i+1] = x
             tmp.append(n/x)
             tmp.append(x)
-
-            #i = i + 1
     print(tmp)
 
-gf(6)
+# destroy any duplicate number in array
+def delN(arr):
+    l = len(arr)
+    tmp = arr
+    tmpN = -1
+    meet = 0
+    incre = 0
+    for x in range(0, l, 1):
+        meet = 0
+        tmpN = arr[x]
+        for y in range(0, l, 1):
+            if arr[x] == arr[y]:
+                meet = meet +1
+            if meet == 2:
+                tmp[y] = -1
+                incre = incre + 1
+                break
+    #fix = tmp
+    #for x in range(0, incre, 1):
+    tmp.remove(-1)
+
+    return tmp
+
+
+a = [4,5,6,7,2,4,8,5,4]
+print(delN(a))
+#gf(6)
 #print(gt(11))
 
